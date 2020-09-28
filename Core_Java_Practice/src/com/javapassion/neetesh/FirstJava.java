3bd6b8f12c3463824eb2b30140d3a8c237d5164f
neeteshtiwari <tiwari.neetesh@gmail.com>
2020-09-28 15:09:21 +0530

    Added a class

---
+++ Core_Java_Practice/src/com/javapassion/neetesh/FirstJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.javapassion.neetesh;
+
+/**
+ * @author mapit
+ *
+ */
+public class FirstJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
